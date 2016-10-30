47d779d73999a02711e8a63d9d79c87760331b65
Craig Duncan <git@duncanc.co.uk>
2016-10-30 16:25:26 +0000

    Create the Intruder class

---
+++ src/Intruder.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace duncan3dc\ObjectIntruder;
+
+class Intruder
+{
+    /**
+     * @var object $instance The object to intrude.
+     */
+    private $_intruderInstance;
+
+    /**
+     * @var \ReflectionClass $reflection The reflected instance.
+     */
+    private $_intruderReflection;
+
+
+    /**
+     * Create a new instance.
+     *
+     * @param object $instance The object to intrude.
+     */
+    public function __construct($instance)
+    {
+        if (!is_object($instance)) {
+            throw new \InvalidArgumentException("Only objects can be intruded");
+        }
+
+        $this->_intruderInstance = $instance;
+    }
+
+
+    private function getInstance()
+    {
+        return $this->_intruderInstance;
+    }
+
+
+    private function getReflection()
+    {
+        if ($this->_intruderReflection === null) {
+            $this->_intruderReflection = new \ReflectionClass($this->_intruderInstance);
+        }
+
+        return $this->_intruderReflection;
+    }
+}
